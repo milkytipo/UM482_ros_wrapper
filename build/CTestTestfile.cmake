537a9e85e107dc515cb65c9db7d3e93faaea1fed
Zheng Gong <gongzheng.joey@gmail.com>
2019-03-10 21:35:16 +0800

    reform the cakin path

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/uav/serialPort/src
-# Build directory: /home/uav/serialPort/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(um482_ros_wrapper)
--- build/um482_ros_wrapper/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/uav/serialPort/src/um482_ros_wrapper
-# Build directory: /home/uav/serialPort/build/um482_ros_wrapper
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
